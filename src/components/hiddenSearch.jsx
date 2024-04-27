b193480f90b11ecf4faedbe17874cd501a1402bb
Wung Brandon <wungbrandon27@gmail.com>
2024-04-27 02:14:05 +0200

    initial commit

---
+++ src/components/hiddenSearch.jsx
@@ -0,0 +1,34 @@
+import './hidden.css'
+import { useState } from 'react'
+
+
+
+
+export default function HiddenSearch(){
+    const [showInput, setShowInput] = useState(false)
+    const [bgColor, setBgColor] = useState("white")
+
+    function handleClick(e){
+        setBgColor("#1a1a1a")
+        if(e.target.className === "container"){
+            setShowInput(false)
+            setBgColor("white")
+        }
+
+
+    }
+
+    return(
+        <div>
+            <section className="container" style={{backgroundColor : bgColor}} onClick={handleClick}>
+                {showInput ? (
+                    <input type="text" placeholder='Search...'/>
+                ) : 
+                (
+                    <button onClick={() => setShowInput(true)}>Search</button>
+                )}
+            </section>
+            <h1>hello</h1>
+        </div>
+    )
+}
